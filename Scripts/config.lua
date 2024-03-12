5ec7d6e69aed8a2299cc1cf19f407030e9db178b
Jordan Addams <43590463+koenjicode@users.noreply.github.com>
2024-03-12 00:02:37 +0000

    text changes in config

--- Scripts/config.lua
+++ Scripts/config.lua
@@ -1,12 +1,11 @@
 -- Here you can adjust the settings for Tranquility v1.2.0!
 
 local _mod_config = {
-    -- Disables Tranquility, regardless of settings.
-    -- Added to Tranquility with v.1.1.1!
+    -- Disables Tranquility, regardless of settings (Added to Tranquility with v.1.1.1).
     disable_everything = true,
 
-    -- Disables players names from showing.
-    -- This does NOT apply the changes on the other player side, the other player will still be able to see your name, these changes are purely client side.
+    -- Disables player names from showing.
+    -- This does NOT apply the changes on the other player's client, the other player will still be able to see your name as these changes are purely client side (like everything else in this mod!).
     streamer_mode = false,
 
     -- Hides player ranks from showing.
@@ -17,7 +16,7 @@ local _mod_config = {
     -- These are the panels shown in Battle, not at the start of the match, if you want to hide the Loading Panels, set "hide_makuai_info" to true.
     hide_player_panels = false,
 
-    -- The distance that the panel will be moved to when rank information is hidden.
+    -- Adjusts the distance of the Player Panels from their default position if rank information is hidden.
     -- Default: 285, I wouldn't really change this, but the option is there you know?
     panel_offset = 285,
 
@@ -28,7 +27,7 @@ local _mod_config = {
     -- Hide Rank Progress bar.
     hide_progress_bar = false,
 
-    -- If a rank promotions occurs, it won't show it.
+    -- If a rank promotion occurs, it won't show it (You've been promoted to this rank etc;).
     -- This does NOT hide the "This is the first time you've reached this rank". Blame the Tekken Team for that. :(
     hide_rank_promotions = false,
 
@@ -36,12 +35,10 @@ local _mod_config = {
     -- This does NOT hide the text underneath ranks. To hide that set "hide_player_ranks" to true.
     hide_rank_prompts = true,
 
-    -- Hides Win Streaks that appear at the bottom of the screen.
-    -- Added to Tranquility with v.1.2.0!
+    -- Hides Win Streaks that appear at the bottom of the screen (Added to Tranquility with v.1.2.0).
     hide_win_streaks = true,
 
-    -- Hides the Makuai stats all together.
-    -- This in particular refers to the information that comes up during the "GET READY FOR THE NEXT BATTLE" screen.
+    -- Hides Player Stats info on the "GET READY FOR THE NEXT BATTLE" screen.
     hide_makuai_info = false,
 }
 
